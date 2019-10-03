2bc3df42f5960df9e476c14476a681b2bd5461c6
arisara45 <52688265+arisara45@users.noreply.github.com>
2019-10-03 12:59:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
-Usertext = decoded [' queryresult '][' เจตนา '][' name ']
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    Usertext = decoded [' queryresult '][' เจตนา '][' name ']
     if(user,userText)
     if(userText == 'สวัสดี') :
         sendText(user,'ดีด้วย')
